c29f6ba8734c3a461e9c426a7c869bb6e8f27e46
bcoop <bcoop@live.unc.edu>
2022-04-07 18:58:13 -0400

    processing args now and brought in coin functions

--- server.js
+++ server.js
@@ -1,9 +1,59 @@
 const express = require('express')
 const app = express()
 
-// could be command line argument...
-var port = 5000
+// get command line args
+import minimist from 'minimist';
+const args = minimist(process.argv.slice(2));
 
-app.listen(port, () => {
+// could be command line argument...
+const port = args.port || process.env.PORT || 5000
+
+// make the server
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}`)
-  })
+  });
+
+// bring all the functions from coin assignment
+function coinFlip() {
+  if (Math.random() > 0.5) {
+    return "heads";
+  } else {
+    return "tails";
+  }
+}
+
+function coinFlips(flips) {
+  const res = [];
+  for(let step = 0; step < flips; step++) {
+    res[step] = coinFlip();
+  }
+  return res;
+}
+
+function countFlips(array) {
+  let head_count = 0;
+  let tail_count = 0;
+  for(let step = 0; step < array.length; step++) {
+    if (array[step] == "heads") {
+      head_count++;
+    } else if (array[step] == "tails") {
+      tail_count++;
+    }
+  }
+  if (head_count == 0) {
+    return {'tails': tail_count}
+  } else if (tail_count == 0) {
+    return {'heads': head_count}
+  } else {
+    return {'heads': head_count, 'tails': tail_count}
+  }
+}
+
+function flipACoin(call) {
+  let flipped = coinFlip();
+  let result = "lose";
+  if (flipped == call) {
+    result = "win";
+  }
+  return "{ call: " + call + ", flip: " + flipped + ", result: " + result + " }";
+}
